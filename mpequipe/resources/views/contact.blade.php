58a6ee83ca7f9f79425fd334c0d39892f6e56865
AS-BAGHDAD <as-baghdad@MacBook-Air-de-AS-BAGHDAD.local>
2019-10-15 15:23:26 +0200

    modif contact

--- mpequipe/resources/views/contact.blade.php
+++ mpequipe/resources/views/contact.blade.php
@@ -4,10 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>TROC</title>
+    <link rel="stylesheet" href="../public/assets/css/style-commun.css">
+    <link rel="stylesheet" href="../public/assets/css/style-accueil.css">
+    <title>TROC HOUR</title>
 </head>
 <body>
-<head>
+<header>
+    <!--<h1>PROJET MARKETPLACE</h1>-->
+    <a href="#"  id="logo"><img src="../public/assets/images/logo.png"></a>
+    <div>
+        <div id="boutonsConnexion">
+            <button type="submit" id="inscription">S'INSCRIRE</button>
+            <button type="submit" id="connexion">SE CONNECTER</button>
+        </div>
+        <nav>
+            <ul id="menu">
+                <li><a href="<?php echo url("/") ?>">ACCUEIL</a></li>
+                <li><a href="<?php echo url("/infos") ?>">COMMENT ÇA MARCHE?</a></li>
+                <li><a href="<?php echo url("/contact") ?>">CONTACT</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
     <h1>PROJET MARKETPLACE</h1>
     <a href="#"><img src="" id="logo"></a>
     <nav>
@@ -35,17 +53,15 @@
 
 </main>
 <footer>
-    <p>XXX est une initiative de la Maison du quartier. Elle est soutenue par la ville de Marseille.</p>
-    <p>Vous pouvez nous soumettre des idées d’améliorations, suggérer des services manquants ou signaler un abus : contact@labanquedetemps.com</p>
-    <a href="#">Mentions légales</a>
-    <a href="#">Conditions Générales d'Utilisation</a>
+    <p>Troc Hour est une initiative de la Maison du quartier. Elle est soutenue par la ville de Marseille.</p>
+    <p>Vous pouvez nous soumettre des idées d’améliorations, suggérer des services manquants ou signaler un abus : contact@troc-hour.com</p>
+    <a href="<?php echo url("/mentions-legales") ?>">Mentions légales</a>
+    <a href="<?php echo url("/conditions-generales") ?>">Conditions Générales d'Utilisation</a>
     <ul>
         <li><a href="<?php echo url("/espace-membre") ?>">membre</a></li>
         <li><a href="<?php echo url("/espace-admin") ?>">admin</a></li>
     </ul>
 </footer>
     
-</body>
-</html>
 </body>
 </html>
