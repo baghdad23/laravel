3daa119f77b20fe0d06421d1bf1e550b588d2cbc
AS-BAGHDAD <as-baghdad@MacBook-Air-de-AS-BAGHDAD.local>
2019-10-15 14:32:07 +0200

    style-css c-g contact

--- mpequipe/resources/views/contact.blade.php
+++ mpequipe/resources/views/contact.blade.php
@@ -22,6 +22,16 @@
 </head>
 <main>
     <h3>SECTION CONTAC</h3>
+    <form method="GET" action="url-traitement.php">
+                <label for="contact-nom">nom</label>
+                <input type="text" placeholder="entrez votre nom" name="nom" required id="contact-nom">
+                <label for="contact-email">email</label>
+                <input type="email" placeholder="entrez votre email" name="email" required id="contact-email">
+                <label for="contact-message">message</label>
+                <textarea name="contact-message" id="contact-message" placeholder="entrez votre message" cols="80" rows="6" required></textarea>
+                <button type="submit">envoyer le message</button>
+            </form>
+        </section>
 
 </main>
 <footer>
