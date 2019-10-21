12bbb2fcd4e68ba57176c90e343f8141c89d87c6
christellegandal <christellegandal@laposte.net>
2019-10-22 01:32:22 +0200

    styleinfo info

--- mpequipe/resources/views/infos.blade.php
+++ mpequipe/resources/views/infos.blade.php
@@ -5,13 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../public/assets/css/style-commun.css">
-    <link rel="stylesheet" href="../public/assets/css/style-accueil.css">
+    <link rel="stylesheet" href="../public/assets/css/style-mentions-légales.css">
+    <link rel="stylesheet" href="../public/assets/css/style-infos.css">
+
     <title>TROC HOUR</title>
 </head>
 <body>
 <header>
     <!--<h1>PROJET MARKETPLACE</h1>-->
-    <a href="#"  id="logo"><img src="../public/assets/images/logo.png"></a>
+    <a href="#"  id="logo"><img src="../public/assets/images/logo.png" style=></a>
     <div>
         <div id="boutonsConnexion">
             <button type="submit" id="inscription">S'INSCRIRE</button>
@@ -28,51 +30,25 @@
 </header>
     <h3>Comment ça marche ?</h3>
 <main>
-    <section id="notice">
-    <div class="col-sm-6 col-md-4">
-            <div class="thumbnail no-borders text-center">
-                <div class="ccm-img"></div>
-                <div class="caption">
-                    <p class="color-white">
-                        1. Laïla s’inscrit <strong>gratuitement</strong> à la Banque de Temps, soit dans les lieux relais, soit sur l'appli web. Elle communique les types de services qu‘elle offre aux autres et ceux dont elle a besoin.
-                    </p>
-                </div>
-            </div>
-        </div>
-            <div class="thumbnail no-borders text-center">
-                <div class="ccm-img"></div>
-                <div class="caption">
-                    <p class="color-white">
-                        1. Laïla s’inscrit <strong>gratuitement</strong> à la Banque de Temps, soit dans les lieux relais, soit sur l'appli web. Elle communique les types de services qu‘elle offre aux autres et ceux dont elle a besoin.
-                    </p>
-                </div>
-            </div>
-        
-
-            <div class="thumbnail no-borders text-center">
-                <div class="ccm-img"></div>
-                <div class="caption">
-                    <p class="color-white">
-                        1. Laïla s’inscrit <strong>gratuitement</strong> à la Banque de Temps, soit dans les lieux relais, soit sur l'appli web. Elle communique les types de services qu‘elle offre aux autres et ceux dont elle a besoin.
-                    </p>
-                </div>
-            </div>
-        
-    <div class="thumbnail no-borders text-center">
-                <div class="ccm-img"></div>
-                <div class="caption">
-                    <p class="color-white">
-                        1. Laïla s’inscrit <strong>gratuitement</strong> à la Banque de Temps, soit dans les lieux relais, soit sur l'appli web. Elle communique les types de services qu‘elle offre aux autres et ceux dont elle a besoin.
-                    </p>
-                </div>
-            </div>
-                <p>Utilisateurs: voisins du quartier.
-User Case:  Sophie, 30 ans, elle est Atsem dans l’école maternelle du quartier. 
-Elle s’inscrit gratuitement à la Banque de Temps. Elle communique les types de services qu‘elle offre aux autres et ceux dont elle a besoin. 
-Elle reçoit 3 crédits (3 heures). Chaque crédit équivaut à une heure de service échangé. Elle consulte les services proposés et elle contacte Paul pour qu’il l’aide à jardiner. Elle dépense un crédit pour 1h de service. 
-Paul utilise son crédit pour recevoir 1h de cours de peinture proposé par Hayat.
-Hayat à son tour choisit de dépenser son crédit pour une heure de cuisine faite par Mario.  
-Mario choisit Abdel pour la garde de ses enfants.. </p>
+    <!-- <section id="notice"> -->
+    <div id="conteneur">
+        <img id="sophie" src="../public/assets/images/sophie.png">            
+            <p>Sophie, 30 ans, elle est Atsem dans l’école maternelle du quartier. 
+            Elle s’inscrit <strong>gratuitement</strong> à troc'hour. Elle communique les types de services qu‘elle offre aux autres et ceux dont elle a besoin.</p>
+        <img src="../public/assets/images/sophie2.png">
+            <p>Elle reçoit 3 crédits (3 heures). Chaque crédit équivaut à une heure de service échangé. Elle consulte les services proposés... </p>
+        <img src="../public/assets/images/sophiePaul.png">
+            <p>et elle contacte Paul pour qu’il l’aide à jardiner. Elle dépense un crédit pour 1h de service.</p>
+    </div>
+    <br>
+    <div id="conteneur1">
+        <img src="../public/assets/images/paulHayat.png">
+            <p>Paul utilise son crédit pour recevoir 1h de cours de peinture proposé par Hayat.</p>
+        <img src="../public/assets/images/hayatMario.png">
+            <p>Hayat à son tour choisit de dépenser son crédit pour une heure de cuisine faite par Mario.</p>
+        <img src="../public/assets/images/MarioAbdel.png">
+            <p>Mario choisit Abdel pour la garde de ses enfants.. </p>
+    </div>
 </main>
 <footer>
     <p>Troc Hour est une initiative de la Maison du quartier. Elle est soutenue par la ville de Marseille.</p>
