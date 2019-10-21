bf1a97429f9ffb6a7a91e040fdd6af9f1399c427
christellegandal <christellegandal@laposte.net>
2019-10-21 16:58:40 +0200

    notice

--- mpequipe/resources/views/infos.blade.php
+++ mpequipe/resources/views/infos.blade.php
@@ -26,20 +26,53 @@
         </nav>
     </div>
 </header>
-    <h1>COMMENT CA MARCHE ?</h1>
-    <a href="#"><img src="" id="logo"></a>
-    <nav>
-        <ul>
-            <li><a href="<?php echo url("/") ?>">accueil</a></li>
-            <li><a href="<?php echo url("/infos") ?>">Comment ça marche?</a></li>
-            <li><a href="<?php echo url("/contact") ?>">contact</a></li>
-        </ul>
-    </nav>
-    <button type="submit" id="inscription">S'INSCRIRE</button>
-    <button type="submit" id="connexion">SE CONNECTER</button>
-</head>
+    <h3>Comment ça marche ?</h3>
 <main>
-    <h3>SECTION INFOS</h3>
+    <section id="notice">
+    <div class="col-sm-6 col-md-4">
+            <div class="thumbnail no-borders text-center">
+                <div class="ccm-img"></div>
+                <div class="caption">
+                    <p class="color-white">
+                        1. Laïla s’inscrit <strong>gratuitement</strong> à la Banque de Temps, soit dans les lieux relais, soit sur l'appli web. Elle communique les types de services qu‘elle offre aux autres et ceux dont elle a besoin.
+                    </p>
+                </div>
+            </div>
+        </div>
+            <div class="thumbnail no-borders text-center">
+                <div class="ccm-img"></div>
+                <div class="caption">
+                    <p class="color-white">
+                        1. Laïla s’inscrit <strong>gratuitement</strong> à la Banque de Temps, soit dans les lieux relais, soit sur l'appli web. Elle communique les types de services qu‘elle offre aux autres et ceux dont elle a besoin.
+                    </p>
+                </div>
+            </div>
+        
+
+            <div class="thumbnail no-borders text-center">
+                <div class="ccm-img"></div>
+                <div class="caption">
+                    <p class="color-white">
+                        1. Laïla s’inscrit <strong>gratuitement</strong> à la Banque de Temps, soit dans les lieux relais, soit sur l'appli web. Elle communique les types de services qu‘elle offre aux autres et ceux dont elle a besoin.
+                    </p>
+                </div>
+            </div>
+        
+    <div class="thumbnail no-borders text-center">
+                <div class="ccm-img"></div>
+                <div class="caption">
+                    <p class="color-white">
+                        1. Laïla s’inscrit <strong>gratuitement</strong> à la Banque de Temps, soit dans les lieux relais, soit sur l'appli web. Elle communique les types de services qu‘elle offre aux autres et ceux dont elle a besoin.
+                    </p>
+                </div>
+            </div>
+                <p>Utilisateurs: voisins du quartier.
+User Case:  Sophie, 30 ans, elle est Atsem dans l’école maternelle du quartier. 
+Elle s’inscrit gratuitement à la Banque de Temps. Elle communique les types de services qu‘elle offre aux autres et ceux dont elle a besoin. 
+Elle reçoit 3 crédits (3 heures). Chaque crédit équivaut à une heure de service échangé. Elle consulte les services proposés et elle contacte Paul pour qu’il l’aide à jardiner. Elle dépense un crédit pour 1h de service. 
+Paul utilise son crédit pour recevoir 1h de cours de peinture proposé par Hayat.
+Hayat à son tour choisit de dépenser son crédit pour une heure de cuisine faite par Mario.  
+Mario choisit Abdel pour la garde de ses enfants.. </p>
 </main>
 <footer>
     <p>Troc Hour est une initiative de la Maison du quartier. Elle est soutenue par la ville de Marseille.</p>
