a6016f78d16f823492852a2216bad7087506c8c6
Derbeca <derbeca@hotmail.com>
2019-10-21 16:44:46 +0200

    inscription et header

--- mpequipe/resources/views/inscription.blade.php
+++ mpequipe/resources/views/inscription.blade.php
@@ -17,8 +17,8 @@
     <a href="#"  id="logo"><img src="../public/assets/images/logo.png"></a>
     <div>
         <div id="boutonsConnexion">
-            <button type="submit" id="inscription">S'INSCRIRE</button>
-            <button type="submit" id="connexion">SE CONNECTER</button>
+            <a href="<?php echo url("/inscription") ?>">S'INSCRIRE</a>
+            <a href="<?php echo url("/connexion") ?>">SE CONNECTER</a>
         </div>
         <nav>
             <ul id="menu">
@@ -30,9 +30,9 @@
     </div>
 </header>
 
-<h3>Inscription</h3>
+<h3>INSCRIPTION</h3>
 
-    <form action method="post" action="">
+    <!-- <form action method="post" action="">
 
     <input type="text" name="nom" id="nom" required placeholder="Nom"/>
     <input type="text" name="pseudo" id="pseudo" required placeholder="Pseudo"/>
@@ -52,7 +52,80 @@
     <ul>
         <li><a href="<?php echo url("/espace-membre") ?>">membre</a></li>
         <li><a href="<?php echo url("/espace-admin") ?>">admin</a></li>
-    </ul>
+    </ul> -->
+
+    <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">Nom</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirmer Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn-inscription">
+                                    S'INSCRIRE
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </footer>
 
 </body>
--- mpequipe/resources/views/layouts/app.blade.php
+++ mpequipe/resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="../public/assets/css/style-inscription.css" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -23,14 +24,14 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+               <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
-                </button>
+                </button> -->
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
 
