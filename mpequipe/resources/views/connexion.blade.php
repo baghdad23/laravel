04b0fd0bc54c81547c4b8c06a9a516303a72f739
florence <gomis211@gmail.com>
2019-10-21 12:04:50 +0200

    ajout style-connexion,style-inscription, style-validation,

--- mpequipe/resources/views/connexion.blade.php
+++ mpequipe/resources/views/connexion.blade.php
@@ -4,62 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../public/assets/css/style-connexion.css">
     <title>TROC</title>
-
-    <style>
-
-
-body{
-    margin: 0;
-    padding: 0;
-    -webkit-background-size: cover;
-    background-size: cover;
-    width: 20%;
-    font-family: Arial, Helvetica, sans-serif;
-    background-color: #D5B772;
-}
-
-input{
-    width: 100%;
-    display: block;
-    margin: 1px auto;
-    padding: 15px;
-    background-color: #546A79; 
-
-input[type=submit]{
-    font-size:16px;
-    background-color: width;
-    border-radius: 5px;
-    }  
-
-.input[type=submit]{
-    font-size:16px;
-    color: #fff;
-    background-color: #546A79;
-    border-radius: 5px;
-
-}
-
-}
-.bg-modal {
-    width: 100%;
-    height: 100%;
-    background-color: rgbo(0, 0, 0, 0, 0.7);
-    position: absolute;
-    top: 0; 
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-.modal-content{
-    width: 500px;
-    height: 300px;
-    background-color: #DSB772;
-    border-radius: 100px;
-}
-
-    </style>
 
 </head>
 <body>
@@ -75,7 +21,8 @@ input[type=submit]{
 
     <input type="email" name="email" placeholder="Email"><br>
     <input type="password" name="mdp" placeholder="Mot de passe"><br>
-    <input type="submit" value="Se connecter">
+    <button type="submit">CONNEXION</button>
+
     @csrf
 
 </form>
--- mpequipe/resources/views/validation.blade.php
+++ mpequipe/resources/views/validation.blade.php
@@ -4,93 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../public/assets/css/style-validation.css">
     <title>Document</title>
-    <style>
-
-body{
-    -webkit-background-size: cover;
-    background-size: cover;
-    font-family: Arial, Helvetica, sans-serif;
-    background-color: #D5B772;
-}
-
-form{
-    padding:10px;
-    text-align: center;
-    margin-left: auto;
-    margin-right: auto;
-}
-
-#rating{
-    width: 0;
-    height: 0;
-
-}
-
-#rating, #contact-message, #pseudo,{
-    margin: 40px;
-    display: inline;
-    position: relative;
-    margin-left: auto;
-    margin-right: auto;
-    text-align: center;
-    height: 30px;
-    width: 50%;
-    flex-direction: column-reverse;
-
-   /* .rating a {
-   color: #aaa;
-   text-decoration: none;
-   font-size: 3em;
-   transition: color .4s;*/
-}
-.rating a:hover,
-.rating a:focus {
-   color: #546A79;
-   cursor: pointer;
-}
-
-.rating a:hover ~ a,
-.rating a:focus ~ a {
-   color: #546A79;
-}
-
-.rating a {
-   float: right;
-   color: #aaa;
-   text-decoration: none;
-   font-size: 3em;
-   transition: color .4s;
-
-}
-
-.rating a {
-   float: right;
-   color: #aaa;
-   text-decoration: none;
-   font-size: 3em;
-   transition: color .4s;
-}
-.rating a:hover,
-.rating a:focus,
-.rating a:hover ~ a,
-.rating a:focus ~ a {
-   color: #546A79;
-   cursor: pointer;
-}
-input[type=submit]{
-    font-size:16px;
-    color: #fff;
-    background-color: #546A79;
-    border-radius: 5px;
-    }
-    </style>
+   
 </head>
 <body>
 
     <h3>Validation</h3>
 
-<form method="GET" action="url-traitement.php">
+<form method="post" action="url-traitement.php">
 
 <div class="rating">
    <a href="#5" title="Donner 5 étoiles">☆</a>
@@ -101,7 +23,7 @@ input[type=submit]{
 
 </div>
         <input type="text" name="pseudo" id="pseudo" required placeholder="Pseudo"/>
-        <textarea name="contact-message" id="contact-message" placeholder="entrez votre message" cols="80" rows="6" required></textarea>
+        <textarea name="contact-message" id="contact-message" placeholder="Entrez votre message" cols="80" rows="6" required></textarea>
         <button type="submit">VALIDATION</button>
         @csrf
 
