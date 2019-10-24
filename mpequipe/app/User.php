0c00533cd1c91e537234b6613dc10cce9f6c7ac0
Derbeca <derbeca@hotmail.com>
2019-10-24 09:35:05 +0200

    ajout colonne photo table users

--- mpequipe/app/User.php
+++ mpequipe/app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'level',
+        'name', 'email', 'password', 'level', 'photo',
     ];
 
     /**
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getImageAttribute()
+    {
+    return $this->photo;
+    }
 }
