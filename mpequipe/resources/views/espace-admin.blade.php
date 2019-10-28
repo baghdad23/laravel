40f3c3b1852ec7fdb130a3d785bf0b9e22382cc3
Derbeca <derbeca@hotmail.com>
2019-10-28 10:14:06 +0100

    change taille des images

--- mpequipe/resources/views/espace-admin.blade.php
+++ mpequipe/resources/views/espace-admin.blade.php
@@ -5,28 +5,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../public/assets/css/style-commun.css">
-    <link rel="stylesheet" href="../public/assets/css/style-accueil.css">
+    <link rel="stylesheet" href="../public/assets/css/style-espace-membre.css">
     <title>TROC HOUR</title>
 </head>
 <body>
 <header>
     <!--<h1>PROJET MARKETPLACE</h1>-->
-    <a href="#"  id="logo"><img src="../public/assets/images/logo.png"></a>
+    <a href="<?php echo url("/") ?>"  id="logo"><img src="../public/assets/images/logo.png"></a>
     <div>
         <div id="boutonsConnexion">
-            <button type="submit" id="inscription">S'INSCRIRE</button>
-            <button type="submit" id="connexion">SE CONNECTER</button>
+            <a href="<?php echo url("/inscription") ?>">S'INSCRIRE</a>
+            <a href="<?php echo url("/connexion") ?>">SE CONNECTER</a>
         </div>
         <nav>
             <ul id="menu">
                 <li><a href="<?php echo url("/") ?>">ACCUEIL</a></li>
                 <li><a href="<?php echo url("/infos") ?>">COMMENT ÇA MARCHE?</a></li>
+                <li><a href="<?php echo url('/espace-membre') ?>">ESPACE MEMBRE</a></li>
                 <li><a href="<?php echo url("/contact") ?>">CONTACT</a></li>
             </ul>
         </nav>
     </div>
 </header>
-
+<main>
+    <div class="espaceMembreForm">
+        <h4>MODIFIER LEVEL MEMBRE</h4>
+        <form method="POST" action="" enctype="multipart/form-data">  
+            <input type="texte" name="name" placeholder="entrez votre nom" required>
+            <input type="email" name="email" placeholder="entrez votre email" required>
+            <input type="password" name="pasword" placeholder="entrez votre mot de passe" required>
+            <input type="file" name="photo">
+            <button type="submit">CHANGER DONNÉES</button>
+            @csrf
+        </form>
+    </div>
+</main>
 <footer>
     <p>Troc Hour est une initiative de la Maison du quartier. Elle est soutenue par la ville de Marseille.</p>
     <p>Vous pouvez nous soumettre des idées d’améliorations, suggérer des services manquants ou signaler un abus : contact@troc-hour.com</p>
